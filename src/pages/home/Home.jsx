676f9d6bd168eb97950c04446e9eb87503db6647
Vinaykumar <vinaykumar12264@gmail.com>
2023-10-26 19:19:48 +0530

    initial development

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,22 @@
+import React, { useContext } from 'react'
+import Layout from '../../components/layout/Layout'
+import myContext from '../../context/data/myContext'
+import HeroSection from '../../components/heroSection/heroSection'
+import Filter from '../../components/filter/Filter'
+import ProductCard from '../../components/productCard/ProductCard'
+import Track from '../../components/track/Track'
+import Testimonial from '../../components/testimonial/Testimonial'
+
+function Home() {
+  return (
+    <Layout>
+      <HeroSection/>
+      <Filter />
+      <ProductCard />
+      <Track/>
+      <Testimonial/>
+    </Layout>
+  )
+}
+
+export default Home
